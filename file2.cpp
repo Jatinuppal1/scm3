cdb2c654ad5d4f50324374ec7c0040b7c40439d3
Jatin Uppal <jatinuppal89@gmail.com>
2019-02-28 10:48:48 +0530

    file 2.cpp and file 3.cpp are in C2 level.

---
+++ file2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+class Area()
+{
+	private:
+	float l,b,a,r;
+	public:
+	Area()
+	{
+		cout<<"_____Area Calculator_____\n";
+	}
+	float rect();
+	float sqr();
+	float crcl();
+};
+int main()
+{
+	return 0;
+}
+
--- file3.cpp
+++ file3.cpp
@@ -1,10 +1,25 @@
 #include<iostream>
+class Salary
+{
+	public:
+	int   b_sal;
+	float   g_sal,d,r;
+	Salary()
+	{
+		cout<<"_____Salary Calculation_____";
+	}
+	float salcal()
+	{
+		 d = ( 40.0/100.0 )*( b_sal );
+		 r = ( 20.0 /100.0 *( b_sal );
+	         g_sal = b_sal + d + r ;
+		 return g_sal; 
+	}
+};
 int main()
 {
-	cout<<"____ Gross Salary Calculator____";
-	int b_sal;
-	float g_sal,d,r;
-	cout<<"\nEnter your Basic Salary - ";
-	cin>>b_sal;
+	Salary S1;
+	cout  <<  "\nEnter your Basic Salary - ";
+	cin  >>  S1.b_sal;
 	return 0;
 }
