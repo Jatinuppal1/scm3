b51a7e88a235f45ba6530d78e44ada5532f28ff2
Jatin Uppal <jatinuppal89@gmail.com>
2019-02-28 10:23:52 +0530

    file1.cpp and file3.cpp are in C1 level.

---
+++ file1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main()
+{
+	cout<<"_____Data Entry______";
+        cout<<"Enter your details.";
+	return 0;
+}
---
+++ file3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+	cout<<"____ Gross Salary Calculator____";
+	int b_sal;
+	float g_sal,d,r;
+	cout<<"\nEnter your Basic Salary - ";
+	cin>>b_sal;
+	return 0;
+}
